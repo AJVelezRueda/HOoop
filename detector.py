09ee548cb25481cb0576265e4047ef620deec41a
Lean <anavelezrueda@gmail.com>
2018-11-11 18:46:52 -0300

    ejercicio listo

--- detector.py
+++ detector.py
@@ -3,8 +3,8 @@ class Detector(object):
     def __init__(self):
         #TODO: completar con la inicializacion de los parametros del objeto
         pass
-
     def detectar(self, senal):
-
         #TODO: Completar
-        pass
+        una_senal = senal
+        #
+        return senal
--- medio.py
+++ medio.py
@@ -2,12 +2,15 @@ class Medio(object):
 
     def __init__(self, blancos):
         self.blancos = blancos
-
-
+        
     def reflejar(self, una_senal, tiempo_inicial, tiempo_final):
         """
         Los blancos en el medio reflejan la senal
         """
+        senial_med = [x*0.5 for x in una_senal]
+        if len(senial_med) <= len(una_senal):
+            return senial_med
+        
 
         #TODO reflejar en un medio debe reflejar en todos los blancos de un medio
         #y devolver la senal reflejada
